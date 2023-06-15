e2cc2a991a1620d35579646edcff4b89e44cae6f
AugustynJ <kuba.augustyn2002@gmail.com>
2023-06-15 23:38:16 +0200

    first server (weak af)

---
+++ server.py
@@ -0,0 +1,84 @@
+
+import time
+import network
+import socket
+from machine import Pin
+from data_read import read_card_nr
+
+ssid = ''
+password = ''
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+html = """<!DOCTYPE html>
+<html>
+<head> <title>Pico</title> </head>
+<body>
+<h1>HELLO NIGGA</h1>
+</body>
+</html>
+"""
+
+html_head = """
+<!DOCTYPE html>
+<html>
+<head> <title>Pico</title> </head>
+<body>
+<h1>HELLO MATE</h1>
+<p>ID CARD = """
+
+html_tail = """
+</body>
+</html>
+"""
+
+# Wait for connect or fail
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+    
+# Handle connection error
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('Connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )
+    
+    
+# Open socket
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen(1)
+print('listening on', addr)
+
+# Listen for connections, serve client
+while True:
+    try:       
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
+        print("request:")
+        print(request)
+        request = str(request)
+        
+        # Create and send response
+        if read_card_nr() == None:
+            response = html
+        else:
+            response = html_head + read_card_nr() + html_tail
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+        
+    except OSError as e:
+        cl.close()
+        print('connection closed')
